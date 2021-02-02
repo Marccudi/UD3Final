d4d59bddc3a2402743641289cea3e3036cdd7071
Marccudi <45258590+Marccudi@users.noreply.github.com>
2021-02-02 16:06:23 +0100

    Final

---
+++ UD2/src/Ej3.java
@@ -0,0 +1,15 @@
+
+public class Ej3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("Cucu");*/
+		for (int i=0 ; i<6 ;/*Ahora viene el paso*/ i++ ) {
+			System.out.print(i + " ");
+			System.out.println("FIN DE PROGRAMA");
+		}
+	}
+
+}
